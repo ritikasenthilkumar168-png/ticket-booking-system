3e43ae1b2543ca3d5794637d3c828663bac1d735
ritikasenthilkumar168-png <ritikasenthilkumar168@gmail.com>
2026-06-30 23:51:24 +0530

    Add toggle functionality to show/hide event details

--- level4/script.js
+++ level4/script.js
@@ -1,3 +1,4 @@
+// Task 1: Toggle booking status
 function toggleBooked(button) {
     if (button.textContent==="Book Ticket") {
         button.textContent="Booked✔️";
@@ -6,4 +7,14 @@ function toggleBooked(button) {
         button.textContent="Book Ticket";
         button.style.backgroundColor="#38bdf8";
     }
+}
+
+// Task 2: Show/hide event details
+function toggleDetails(id) {
+  const details = document.getElementById(id);
+  if (details.style.display === "block") {
+    details.style.display = "none";
+  } else {
+    details.style.display = "block";
+  }
 }
